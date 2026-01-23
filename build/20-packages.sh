55bc6e2b1f6a2796d9e78b1b594b96b00b0b5e4e
monty <monty@taintedinfo.com>
2026-01-23 08:01:01 -0500

    fixed syntax error

--- build/20-packages.sh
+++ build/20-packages.sh
@@ -12,16 +12,14 @@ rpm-ostree install \
   alacritty \
   kitty \
   freerdp \
-  google-noto-emoji-fonts
-# --- STANDARD FONTS (Official RPMs) ---
-jetbrains-mono-fonts \
+  google-noto-emoji-fonts \
+  jetbrains-mono-fonts \
   fira-code-fonts \
   adobe-source-code-pro-fonts \
   cascadia-code-fonts \
   google-noto-sans-mono-fonts \
-  google-droid-sans-mono-fonts
-# --------------------------------------
-tailscale \
+  google-droid-sans-mono-fonts \
+  tailscale \
   remmina \
   remmina-plugins-rdp \
   remmina-plugins-vnc \
